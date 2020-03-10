4f50ea54c2e95b8ea6fc7ef2d70c895b2b040fe0
testsabirweb <testsabirweb@gmail.com>
2020-03-10 16:04:06 +0530

    started working on adding Listners

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { FlatList, Platform, Button } from 'react-native'
+import React, { useState, useEffect, useCallback } from 'react'
+import { FlatList, Platform, Button, ActivityIndicator, View, StyleSheet, Text } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
@@ -10,11 +10,24 @@ import HeaderButton from '../../components/UI/HeaderButton'
 import Colors from '../../constants/Colors'
 
 const ProductsOverviewScreen = (props) => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState()
     const products = useSelector((state) => state.products.availableProducts)
     const dispatch = useDispatch()
 
+    const loadProducts = useCallback(async () => {
+        setError(null)
+        setIsLoading(true)
+        try {
+            await dispatch(productActions.fetchProducts())
+        } catch (err) {
+            setError(err.message)
+        }
+        setIsLoading(false)
+    }, [dispatch, setIsLoading, setError])
+
     useEffect(() => {
-        dispatch(productActions.fetchProducts())
+        loadProducts()
     }, [dispatch])
 
     const selectItemHandler = (id, title) => {
@@ -22,6 +35,35 @@ const ProductsOverviewScreen = (props) => {
             productId: id,
             productTitle: title
         })
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size="large" color={Colors.primary} />
+            </View>
+        )
+    }
+
+    if (!isLoading && products.length === 0) {
+        return (
+            <View style={styles.centered}>
+                <Text>No products found,Maybe start adding some in the admin pannel</Text>
+            </View>
+        )
+    }
+
+    if (error) {
+        return (
+            <View style={styles.centered}>
+                <Text>An Error occured</Text>
+                <Button
+                    title="Try Again"
+                    color={Colors.primary}
+                    onPress={loadProducts}
+                />
+            </View>
+        )
     }
 
     return (
@@ -84,5 +126,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
 
 export default ProductsOverviewScreen
--- store/actions/product.js
+++ store/actions/product.js
@@ -7,24 +7,33 @@ export const SET_PRODUCTS = 'SET_PRODUCTS'
 
 export const fetchProducts = () => {
     return async dispatch => {
-        const response = await fetch(
-            'https://shoppy-abcd.firebaseio.com/products.json'
-        )
-        const resData = await response.json()
-        const loadedProducts = []
-        for (const key in resData) {
-            loadedProducts.push(
-                new Product(
-                    key,
-                    'u1',
-                    resData[key].title,
-                    resData[key].imageUrl,
-                    resData[key].description,
-                    resData[key].price
-                )
+        try {
+            const response = await fetch(
+                'https://shoppy-abcd.firebaseio.com/products.json'
             )
+
+            if (!response.ok) {
+                throw new Error('Something went wrong maybe try after some time')
+            }
+
+            const resData = await response.json()
+            const loadedProducts = []
+            for (const key in resData) {
+                loadedProducts.push(
+                    new Product(
+                        key,
+                        'u1',
+                        resData[key].title,
+                        resData[key].imageUrl,
+                        resData[key].description,
+                        resData[key].price
+                    )
+                )
+            }
+            dispatch({ type: SET_PRODUCTS, products: loadedProducts })
+        } catch (err) {
+            throw err
         }
-        dispatch({ type: SET_PRODUCTS, products: loadedProducts })
     }
 }
 
